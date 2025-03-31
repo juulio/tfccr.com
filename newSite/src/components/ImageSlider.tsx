15ed501820f002192af5bfccb684c12a029234ef
Julio Del Valle <juulio@gmail.com>
2025-03-30 23:06:00 -0600

    Styling h2 text inside slider

--- newSite/src/components/ImageSlider.tsx
+++ newSite/src/components/ImageSlider.tsx
@@ -3,9 +3,9 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import './ImageSlider.scss'
-import slider00_asesoriaFinanciera from '/slider_asesoria_financiera.webp'
-import slider01_ejecucionCursos from '/slider_ejecucion_cursos.webp'
-import slider02_experiencia from '/slider_experiencia.webp'
+import slider_asesoriaFinanciera from '/slider_asesoria_financiera.webp'
+import slider_ejecucionCursos from '/slider_ejecucion_cursos.webp'
+import slider_experiencia from '/slider_experiencia.webp'
 
 const ImageSlider: React.FC = () => {
   const settings = {
@@ -30,24 +30,22 @@ const ImageSlider: React.FC = () => {
   return (
     <div className="slider-container">
       <Slider {...settings}>
-        <div>
-          <img src={slider00_asesoriaFinanciera} alt="Slide 1" />
+        <div id="sliderElement00">
+          <img src={slider_asesoriaFinanciera} alt="Slide 1" />
           <h2>
-            Asesoria financiera y<br />
-            estratégica para equipos
-            <br />
+            Asesor&iacute;a financiera y estrat&eacute;gica para equipos
             gerenciales y empresarios
           </h2>
         </div>
-        <div>
-          <img src={slider01_ejecucionCursos} alt="Slide 2" />
+        <div id="sliderElement01">
+          <img src={slider_ejecucionCursos} alt="Slide 2" />
           <h2>
             Recomendación, planificación y coordinación de la ejecución de
             cursos de acción que crean valor para las organizaciones
           </h2>
         </div>
-        <div>
-          <img src={slider02_experiencia} alt="Slide 3" />
+        <div id="sliderElement02">
+          <img src={slider_experiencia} alt="Slide 3" />
           <h2>
             Toda nuestra experiencia puesta al servicio de
             <br />
