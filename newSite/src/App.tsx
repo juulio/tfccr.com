2e6a9f48fc719534e34b3bdd5315bb4c01981a64
Julio Del Valle <juulio@gmail.com>
2025-03-30 21:54:11 -0600

    Add 3 images for top slider

--- newSite/src/App.tsx
+++ newSite/src/App.tsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <Navbar />
-      <h1>My React Slick Slider</h1>
       <ImageSlider />
+      <h1>Acerca de</h1>
     </I18nextProvider>
   )
 }
--- newSite/src/components/ImageSlider.tsx
+++ newSite/src/components/ImageSlider.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import './ImageSlider.css'
 
 const ImageSlider: React.FC = () => {
   const settings = {
