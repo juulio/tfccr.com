2167748383914aceb4177cd38c1024a7916ebedf
Julio Del Valle <juulio@gmail.com>
2025-04-03 08:19:49 -0600

    Arranging file imports

--- newSite/src/main.tsx
+++ newSite/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './reset.scss'
 import App from './App.tsx'
 
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import './reset.scss' // Import CSS Reset
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
