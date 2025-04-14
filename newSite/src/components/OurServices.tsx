3060bfbec4fd3a92051e94c3ca7f7e0ae648aff2
Julio Del Valle <juliode@microsoft.com>
2025-04-14 13:13:53 -0600

    Fixing Our Services styles for mobile first and desktop reorder

--- newSite/src/components/OurServices.tsx
+++ newSite/src/components/OurServices.tsx
@@ -14,40 +14,40 @@ const AboutUs: React.FC = () => {
     <div className="our-services module max-width">
       <h2>{t('titleServices')}</h2>
       <section>
-        <div className="column">
+        <div className="column mobileItem01">
+          <img src={img_valoracionFinanciera} alt="Slide 1" />
+        </div>
+        <div className="column mobileItem02">
           <h3>{t('subtitleValuation')}</h3>
           <p>{t('textValuation')}</p>
         </div>
-        <div className="column">
-          <img src={img_valoracionFinanciera} alt="Slide 1" />
-        </div>
-        <div className="column">
+        <div className="column mobileItem03">
           <img src={img_fusionesAdquisiciones} alt="Slide 1" />
         </div>
-        <div className="column">
+        <div className="column mobileItem04">
           <h3>{t('subtitleMergersAndAcquisitions')}</h3>
           <p>{t('textMergersAndAcquisitions')}</p>
         </div>
-        <div className="column">
+        <div className="column mobileItem05">
+          <img src={img_analisisFinanciero} alt="Slide 1" />
+        </div>
+        <div className="column mobileItem06">
           <h3>{t('subtitleFinancialAnalysis')}</h3>
           <p>{t('textFinancialAnalysis')}</p>
         </div>
-        <div className="column">
-          <img src={img_analisisFinanciero} alt="Slide 1" />
-        </div>
-        <div className="column">
+        <div className="column mobileItem07">
           <img src={img_presupuestacion} alt="Slide 1" />
         </div>
-        <div className="column">
+        <div className="column mobileItem08">
           <h3>{t('subtitleBudgeting')}</h3>
           <p>{t('textBudgeting')}</p>
         </div>
-        <div className="column">
+        <div className="column mobileItem09">
+          <img src={img_servicioAsesoriaJuntasDirectivas} alt="Slide 1" />
+        </div>
+        <div className="column mobileItem10">
           <h3>{t('subtitleBoardOfDirectorsConsulting')}</h3>
           <p>{t('textBoardOfDirectorsConsulting')}</p>
-        </div>
-        <div className="column">
-          <img src={img_servicioAsesoriaJuntasDirectivas} alt="Slide 1" />
         </div>
       </section>
     </div>
